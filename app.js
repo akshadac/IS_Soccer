b543ee2e800b0d35436ea47fb6dd0095696c075b
PrincessAru <princessaru9@gmail.com>
2023-04-06 15:44:51 -0500

    teams are in

---
+++ app.js
@@ -0,0 +1,47 @@
+let Real_Madrid = {
+    team_name: "Real Madrid",
+    city: "Madrid",
+    country: "Spain",
+    top_scorers: ["Ronaldo", "Benzema", "Hazard"],
+    worldwide_fans: 798
+};
+
+let Manchester_United = {
+    team_name: "Manchester United",
+    city: "Manchester",
+    country: "England",
+    top_scorers: ["Cantona", "Rooney", "Ronaldo"],
+    worldwide_fans: 755
+};
+
+let Manchester_City = {
+    team_name: "Manchester City",
+    city: "Manchester",
+    country: "England",
+    top_scorers: ["Sterling", "Aguero", "Haaland"],
+    worldwide_fans: 537
+};
+
+let Brazil_National_Team = {
+    team_name: "Brazil National Team",
+    city: "N/A",
+    country: "Brazil",
+    top_scorers: ["Ronaldinho", "Cafu", "Bebeto"],
+    worldwide_fans: 950
+};
+
+let Argentina_National_Team = {
+    team_name: "Argentina National Team",
+    city: "N/A",
+    country: "Argentina",
+    top_scorers: ["Messi", "Batistuta", "Maradona"],
+    worldwide_fans: 888
+};
+
+let Atletico_Madrid = {
+    team_name: "Atletico Madrid",
+    city: "Madrid",
+    country: "Spain",
+    top_scorers: ["Aragonés", "Griezmann", "Torez"],
+    worldwide_fans: 400
+};
